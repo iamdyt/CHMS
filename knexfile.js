ebbfb030ac8e98da0a736abe193f2ad565727b18
iamdyt <olanlokunlanre@gmai.com>
2019-10-09 08:04:42 -0700

    dbadding

--- knexfile.js
+++ knexfile.js
@@ -2,10 +2,10 @@ options = {
     client:'mysql',
     version:'10.1.38',
     connection:{
-        host:'127.0.0.1',
-        user:'root',
-        password:'',
-        database:'chms'
+        host:'us-cdbr-iron-east-05.cleardb.net',
+        user:'b772dbfa48648d',
+        password:'4ba4a8b5',
+        database:'heroku_4229e7d82e5f8cc'
     },
     migrations:{
         directory:__dirname+'/db/migrations'
@@ -15,5 +15,5 @@ options = {
         directory:__dirname+'/db/seeds'
     }
 };
-
+mysql://b772dbfa48648d:4ba4a8b5@us-cdbr-iron-east-05.cleardb.net/heroku_4229e7d82e5f8cc?reconnect=true
 module.exports = options;
